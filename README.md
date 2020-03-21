13dc0905affd0a374a1b65e2e20f5e59cd524415
Blair Currey <curreyb88@gmail.com>
2020-03-21 17:45:04 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Rabbit hole
+a metaphor for something that transports someone into a wonderfully (or troublingly) surreal state or situation
+#
+Have you ever started watching something on Youtube and ended up engrossed in a completely unrelated video?
+
+Or have you ever read a comment on a video that proclaims something to the effect of "It's 2am on a Tuesday and I have no experience with carpentry but I can't stop learning about Japanese hardware-less joinery techniques"
+
+Well, why not kill all the fun and get straight to the bottom of these time-wasting journeys?
+
+This python program takes a youtube video url as input and a number of "dives" and continues on to the next recommended video that many times. It returns a list of dictionaries which contain data for each video (title, channel name, url, etc).
+
+**Libraries used** 
+
+ - BeautifulSoup
+ - requests
+ - time
+ - json
+	
---
+++ yt-scrape.py
@@ -0,0 +1,109 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import json
+
+def save_to_file(video_dictionary):
+    with open('result.json', 'w') as fp:
+        json.dump(video_dictionary, fp, indent=4)
+
+def youtube_dive(dives, start_video):
+    video_list = []
+
+    for i in range(0, dives):
+        next_video_data = find_next_video(start_video, video_list)
+        video_list.append(next_video_data)
+        start_video = next_video_data['next_link']
+    
+    return video_list
+
+def find_next_video(start_video, history):
+
+    video_data = {
+        "id": len(history) + 1,
+        "title": None,
+        "channel": None,
+        "views": None,
+        "category": None,
+        "link": start_video,
+        "next_link": None
+    }
+
+    # Find the next video html
+    soup = get_soup(start_video, history)
+    next_video_html = get_next_video_html(soup)
+
+    # set timeout and counter 
+    timeout = 6
+    counter = 0
+    # make sure next_video_html exists
+    while not next_video_html:
+        print("Retrying....")
+        
+        counter += 1
+        
+        # try until counter exceeds timeout
+        if counter <= timeout:
+            soup = get_soup(start_video, history)
+            next_video_html = get_next_video_html(soup)
+        
+        # go back to the previous video in history and retry
+        else:
+            previous_video = history[len(history) - 1]
+            print("Timeout. Going back 1 video to" + previous_video)
+            start_video = previous_video
+
+    # get href and make link for next video
+    next_video_href = next_video_html.get('href')
+    video_data['next_link'] = 'https://www.youtube.com' + next_video_href
+
+    # Checks if next link is present in our list of dictionaries (history)
+    # adapted code from https://stackoverflow.com/questions/3897499/check-if-value-already-exists-within-list-of-dictionaries
+    if any(d['link'] == video_data['next_link'] for d in history):
+        print(f"{video_data['next_link']} is a duplicate - Retrying ...")
+        # get all the videos
+        videos_html = get_next_video_html(soup, return_list=True)
+        
+        # make a new possible link from the list of videos
+        for i in range(0, len(videos_html)):
+            possible_new_link = 'https://www.youtube.com' + videos_html[i].get('href')
+            
+            #if the new possible link has not been visited, make it the next video
+            if not any(d['link'] == possible_new_link for d in history):
+                video_data['next_link'] = possible_new_link
+    
+    #get the rest of the data
+    video_data['title'] = soup.find('span', class_='watch-title').text.strip()
+    video_data['channel'] = soup.find('div', class_='yt-user-info').text.strip()
+    video_data['category'] = soup.find('ul', class_='watch-info-tag-list').a.text.strip()
+    video_data['views'] = soup.find(class_='view-count').text.strip()
+
+    print(video_data)
+    return video_data
+
+def get_next_video_html(soup, return_list=None):
+    
+    if return_list == None:
+        return soup.find('a', class_='content-link')
+    else:
+        return soup.find_all('a', class_='content-link') # should work without 'a' argument        
+
+def get_soup(start_video, history):
+    # Get web page
+    page = requests.get(start_video)
+    
+    # Print status code for debugging purposes
+    print(f"[{str(len(history))}] Request status code for {start_video}: {str(page.status_code)}")
+    
+    # Parse HTML
+    return BeautifulSoup(page.content, 'html.parser')
+
+def save_html(soup, url):
+    with open('yt-scrape-html.txt','w',encoding='utf-8-sig') as f:
+        f.write(url + "\n" + soup.prettify())
+
+if __name__ == "__main__":
+
+    #search by video
+    rabbit_hole = youtube_dive(10, start_video="https://www.youtube.com/watch?v=R1KcSa39gkI")
+    save_to_file(rabbit_hole)
