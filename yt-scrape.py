3973d37d7f385c18995ec84ba6732a08bc036084
Blair Currey <curreyb88@gmail.com>
2020-03-25 17:07:49 -0400

    refactor: Created 'Video_data' class, refactored 'Rabbithole class'

--- yt-scrape.py
+++ yt-scrape.py
@@ -27,15 +27,20 @@ class Rabbithole:
         return self.videos
 
     def find_next_video(self, current_video):
+        """
+        Could potentially be refactored to check that soup retrieval was succesful and the 
+        next video html is accesible and not a duplicate. Could move video data structure 
+        and video data retrieval to a new class.
+        """
         # stores data for current video
-        video_data = {
+        video = {
             "id": len(self.videos) + 1,
+            "link": current_video,
+            "next_link": None,
             "title": None,
             "channel": None,
             "views": None,
-            "category": None,
-            "link": current_video,
-            "next_link": None
+            "category": None
         }
 
         # Find the next video html
@@ -64,12 +69,12 @@ class Rabbithole:
 
         # get href and make link for next video
         next_video_href = next_video_html.get('href')
-        video_data['next_link'] = 'https://www.youtube.com' + next_video_href
+        video['next_link'] = 'https://www.youtube.com' + next_video_href
 
         # Checks if next link is present in our list of dictionaries (history)
         # adapted code from https://stackoverflow.com/questions/3897499/check-if-value-already-exists-within-list-of-dictionaries
-        if any(d['link'] == video_data['next_link'] for d in self.videos):
-            print(f"{video_data['next_link']} is a duplicate - Retrying ...")
+        if any(d['link'] == video['next_link'] for d in self.videos):
+            print(f"{video['next_link']} is a duplicate - Retrying ...")
             # get html snippets for all the videos
             videos_html = self.get_next_video_html(soup, return_list=True)
             
@@ -79,16 +84,13 @@ class Rabbithole:
                 
                 #if the new possible link has not been visited, make it the next video
                 if not any(d['link'] == possible_new_link for d in self.videos):
-                    video_data['next_link'] = possible_new_link
+                    video['next_link'] = possible_new_link
         
         #get the rest of the data
-        video_data['title'] = soup.find('span', class_='watch-title').text.strip()
-        video_data['channel'] = soup.find('div', class_='yt-user-info').text.strip()
-        video_data['views'] = soup.find(class_='view-count').text.strip()
-        video_data['category'] = self.get_category(soup)
+        scraped_video = Video_data(video['link'], video['next_link'], soup, len(self.videos))
 
-        print(video_data)
-        return video_data
+        print(scraped_video.data)
+        return scraped_video.data
 
     def get_soup(self, current_video):
         # Get web page
@@ -110,16 +112,68 @@ class Rabbithole:
         else:
             return soup.find_all('a', class_='content-link') # should work without 'a' argument   
 
-    def get_category(self, soup):
-        #find the h4 element with the text 'Category'
-        category_title = soup.find('h4', class_="title", text = re.compile('Category'))
-        parent = category_title.parent
-        #find category name
-        category_value = parent.find('a').text.strip()
-        
-        return category_value
+class Video_data:
+        def __init__(self, start_video, next_video, soup, id_num):
+            self.soup = soup
+            self.data = {
+                "id": id_num,
+                "title": None,
+                "channel": None,
+                "views": None,
+                "category": None,
+                "link": start_video,
+                "next_link": next_video
+            }
+            self.collect_data()
+
+        def collect_data(self):
+            title = self.find_title()
+            title_checked = self.handle_failed_scrape(title)
+            self.data["title"] = title_checked
+
+            channel = self.find_channel()
+            channel_checked = self.handle_failed_scrape(channel)
+            self.data["channel"] = channel_checked
+
+            views = self.find_views()
+            views_checked = self.handle_failed_scrape(views)
+            self.data["views"] = views_checked
+
+            category = self.find_category()
+            category_checked = self.handle_failed_scrape(category)
+            self.data["category"] = category_checked
+
+        def find_title(self):
+            title = self.soup.find('span', class_='watch-title').text.strip()
+            return title
+
+        def find_channel(self):
+            channel = self.soup.find('div', class_='yt-user-info').text.strip()
+            return channel
+
+        def find_views(self):
+            views = self.soup.find(class_='view-count').text.strip()
+            return views
+
+        def find_category(self):
+            #find the h4 element with the text 'Category', then find it's parent
+            category_title = self.soup.find('h4', class_="title", text = re.compile('Category'))
+            parent = category_title.parent
+            #find category name from parent
+            category = parent.find('a').text.strip()
+            return category
+
+        def handle_failed_scrape(self, soup_find_return):
+            if soup_find_return is not None:
+                return soup_find_return
+            else:
+                return "[ERROR] Not Found"
 
 if __name__ == "__main__":
-    
-    basketball = Rabbithole("https://www.youtube.com/watch?v=R1KcSa39gkI", 5)
-    basketball.save_json()
+    examples = {
+        "wolf_hunting": "https://www.youtube.com/watch?v=hMIGbEfikGQ",
+        "larry_bird": "https://www.youtube.com/watch?v=R1KcSa39gkI"
+    }
+
+    wolf_hunting = Rabbithole(examples["wolf_hunting"], 5)
+    wolf_hunting.save_json(name="wolf_hunting")
