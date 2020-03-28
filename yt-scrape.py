65f34435b14d8ea16db0ed1795a40b18062a35a4
Blair Currey <curreyb88@gmail.com>
2020-03-28 14:39:17 -0400

    Converted find_next_video method in Rabbithole class into it's own class and renamed things for clarity

--- yt-scrape.py
+++ yt-scrape.py
@@ -5,127 +5,129 @@ import re
 import json
 
 class RabbitHole:
-    def __init__(self, start_video, dives):
-        self.start_video = start_video
+    def __init__(self, start_video_url, dives):
+        self.start_video_url = start_video_url
         self.dives = dives
-        self.videos = []
+        self.visited_videos = []
         self.go_down_rabbithole()
 
     def save_json(self, name="result"):
         with open(f'results/{name}.json', 'w') as fp:
-            json.dump(self.videos, fp, indent=4)
+            json.dump(self.visited_videos, fp, indent=4)
 
     def go_down_rabbithole(self):
         #grab class start video and set to current video
-        current_video = self.start_video
+        current_video_url = self.start_video_url
 
         for _ in range(0, self.dives):
-            next_video_data = self.find_next_video(current_video)
-            self.videos.append(next_video_data)
-            current_video = next_video_data["next_link"]
+            next_video = NextVideo(current_video_url, self.visited_videos)
+            next_video_scrape = VideoData(next_video.video["url"], 
+                                            next_video.video["next_url"], 
+                                            next_video.video["soup"], 
+                                            next_video.video["id"])
+            self.visited_videos.append(next_video_scrape.data)
+            current_video_url = next_video_scrape.data["next_url"]
         
-        return self.videos
+        return self.visited_videos
 
-    def find_next_video(self, current_video):
-        # stores data for current video
-        video = {
-            "id": len(self.videos) + 1,
-            "link": current_video,
-            "next_link": None,
-            "soup": None
+class NextVideo:
+    def __init__(self, current_video_url, visited_videos):
+        self.visited_videos = visited_videos
+        # self.next_video_html = None
+        self.video = {
+            "id": len(visited_videos) + 1,
+            "url": current_video_url,
+            "next_url": None,
+            "soup": None  
         }
+        self.get_next_video()
 
+    def get_next_video(self):
         # Get soup and find the next video html
-        video["soup"] = self.get_soup(video["link"])
-        next_video_html = self.find_next_video_html(video["soup"])
+        self.video["soup"] = self.get_soup()
+        next_video_html = self.find_next_video_html()
         
-        # Find new video html and soup if first attempt yields None
+        # Find new video html (which also changes self.soup) if first attempt yields None
         if next_video_html is None:
-            next_video_html, video["soup"] = self.retry_find_next_video_html(video["soup"], video["link"], next_video_html)
+            next_video_html = self.retry_find_next_video_html(next_video_html)
 
         # Find new video if it is a YouTube Movie or duplicate
         if self.video_is_youtube_movie(next_video_html) or self.video_is_duplicate(next_video_html):
-            next_video_html = self.pick_next_video_html_from_list(video["soup"])
+            next_video_html = self.pick_next_video_html_from_list()
         
-        video["next_link"] = self.make_link_from_html(next_video_html)
-        
-        #get the rest of the data
-        scraped_video = VideoData(video['link'], video['next_link'], video["soup"], video['id'])
+        self.video["next_url"] = self.make_url_from_html(next_video_html)
 
-        print(scraped_video.data)
-        return scraped_video.data
-    
-    def get_soup(self, current_video):
+    def get_soup(self):
         # Get web page
-        page = requests.get(current_video)
+        page = requests.get(self.video["url"])
         
         # Print status code for debugging purposes
-        print(f"[{len(self.videos) + 1}] Request status code for {current_video}: {str(page.status_code)}")
+        print(f"[{len(self.visited_videos) + 1}] Request status code for {self.video['url']}: {str(page.status_code)}")
         
         # Parse HTML
         return BeautifulSoup(page.content, 'html.parser')
 
-    def find_next_video_html(self, soup):
-        return soup.find('a', class_='content-link')
+    def find_next_video_html(self):
+        return self.video["soup"].find('a', class_='content-link')
         
-    def find_next_video_html_list(self, soup):
-        return soup.find_all('a', class_='content-link')
+    def find_next_video_html_list(self):
+        return self.video["soup"].find_all('a', class_='content-link')
 
-    def retry_find_next_video_html(self, soup, current_video, html):
+    def retry_find_next_video_html(self, next_video_html):
         attempts = 0
         threshold = 5
 
         while attempts < threshold:
             attempts += 1
             
-            if html is None:
+            if next_video_html is None:
                 print("Failed to get next video. Making new soup and Retrying ...")
-                new_soup = self.get_soup(current_video)
-                html = self.find_next_video_html(new_soup)
-                if html:
-                    return html, new_soup
+                self.video["soup"] = self.get_soup()
+                next_video_html = self.find_next_video_html()
+                if next_video_html:
+                    return next_video_html
             else:
                 print(f"Could not find video html in {threshold} attempts")
-    
-    def make_link_from_html(self, next_video_html):
-        href = next_video_html.get('href')
-        return 'https://www.youtube.com' + href
 
-    def video_is_duplicate(self, next_video_html):
-        next_link = self.make_link_from_html(next_video_html)
-        #check if next_link is found in self.videos
-        if any(v['link'] == next_link for v in self.videos):
-            print(f"{next_link} is a duplicate \n Retrying ...")
-            return True
-        else:
-            return False
-    
     def video_is_youtube_movie(self, next_video_html): 
         next_video_html_subtext = next_video_html.find('span', class_='')
         
         if next_video_html_subtext and next_video_html_subtext.text == "YouTube Movies":
-            print("next video html is for a YouTube Movie")
+            print("next video html is for a YouTube Movie \n Retrying ...")
             return True
         else:
-            return False
+            return False  
 
-    def pick_next_video_html_from_list(self, soup):
-        next_video_html_list = self.find_next_video_html_list(soup)
-            
-        for i in range(1, len(next_video_html_list)):
-            new_html = next_video_html_list[i]
-            new_html_is_youtube_movie = self.video_is_youtube_movie(new_html)
-            new_html_is_duplicate = self.video_is_duplicate(new_html)
-
-            if not new_html_is_duplicate and not new_html_is_youtube_movie:
-                return new_html
+    def video_is_duplicate(self, next_video_html):
+            next_url = self.make_url_from_html(next_video_html)
+            #check if next_url is found in the visited videos list
+            if any(v["url"] == next_url for v in self.visited_videos):
+                print(f"{next_url} is a duplicate \n Retrying ...")
+                return True
             else:
-                print("New next video html is not valid. Retrying ...")
-        
-        print("Could not find valid next video html from list")
+                return False  
+
+    def pick_next_video_html_from_list(self):
+            next_video_html_list = self.find_next_video_html_list()
+                
+            for i in range(1, len(next_video_html_list)):
+                new_html = next_video_html_list[i]
+                new_html_is_youtube_movie = self.video_is_youtube_movie(new_html)
+                new_html_is_duplicate = self.video_is_duplicate(new_html)
+
+                if not new_html_is_duplicate and not new_html_is_youtube_movie:
+                    return new_html
+                else:
+                    print("New next video html is not valid. Retrying ...")
+            
+            print("Could not find valid next video html from list")
+                
+    def make_url_from_html(self, next_video_html):
+        href = next_video_html.get('href')
+        return 'https://www.youtube.com' + href
 
 class VideoData:
-        def __init__(self, start_video, next_video, soup, id_num):
+        def __init__(self, start_video_url, next_video_url, soup, id_num):
             self.soup = soup
             self.failed_scrape_message = "[ERROR] Not Found"
             self.data = {
@@ -134,8 +136,8 @@ class VideoData:
                 "channel": None,
                 "views": None,
                 "category": None,
-                "link": start_video,
-                "next_link": next_video
+                "url": start_video_url,
+                "next_url": next_video_url
             }
             self.collect_data()
 
@@ -144,6 +146,7 @@ class VideoData:
             self.data["channel"] = self.find_channel()
             self.data["views"] = self.find_views()
             self.data["category"] = self.find_category()
+            print(self.data)
 
         def find_title(self):
             title = self.soup.find('span', class_='watch-title').text.strip()
